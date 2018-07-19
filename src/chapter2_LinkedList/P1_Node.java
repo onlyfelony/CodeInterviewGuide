a8f8cc944340de8fcf62f57107797582ceb883d8
onlyfelony <1260675642@qq.com>
2018-07-19 19:35:26 +0800

    chapter3

--- src/chapter2_LinkedList/P1_Node.java
+++ src/chapter2_LinkedList/P1_Node.java
@@ -9,7 +9,9 @@ public class P1_Node {
 	public P1_Node next;
 	
 	public P1_Node(int value){
+		
 		this.value = value;
+		
 	}
 	
 }
---
+++ src/chapter3_BinaryTree/SingleNode.java
@@ -0,0 +1,19 @@
+package chapter3_BinaryTree;
+
+/*
+ * 单链表结点
+ */
+
+public class SingleNode {
+
+	public int value;
+
+	public SingleNode next;
+
+	public SingleNode(int value) {
+
+		this.value = value;
+
+	}
+
+}
